e0491c658f9bf1d8147a3463439a4074347c6edc
Ezequiel Bugnon <bugnonezequiel@gmail.com>
2025-08-24 14:26:55 -0300

    feat(app): add folder token

--- REPL/repl.go
+++ REPL/repl.go
@@ -4,12 +4,17 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"leetcode/src/compiler-in-go/lexer"
 )
 
 func Repl(in io.Reader) {
 	scanner := bufio.NewScanner(in)
+	lexer := lexer.New()
 
-	for scanner.Scan() {
+	if scanner.Scan() {
 		fmt.Println("working", scanner.Text())
+		lexer.Apply(scanner.Text())
 	}
+
+	fmt.Println("lexer", lexer)
 }
--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,7 +1,14 @@
 package lexer
 
 const EQUAL = "="
+const PLUS = "+"
+const MINUS = "-"
+const LET = "LET"
 const EOF = "EOF"
+const IF = "IF"
+const ELSE = "ELSE"
+const LEFTBRACKET = "("
+const RIGHTBRACKET = ")"
 
 type lexer struct {
 	Identifier string
@@ -10,10 +17,23 @@ type lexer struct {
 
 type lexerImp []lexer
 
+func New() *lexerImp {
+	return &lexerImp{}
+}
+
 func (l *lexerImp) Apply(lexer string) {
+
 	switch lexer {
 	case "=":
 		*l = append(*l, addLexer(EQUAL, "="))
+	case "+":
+		*l = append(*l, addLexer(PLUS, "+"))
+	case "-":
+		*l = append(*l, addLexer(MINUS, "-"))
+	case "(":
+		*l = append(*l, addLexer(LEFTBRACKET, "("))
+	case "":
+		*l = append(*l, addLexer(RIGHTBRACKET, ")"))
 	default:
 		*l = append(*l, addLexer(EOF, "EOF"))
 	}
