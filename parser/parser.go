630b3c93cc914c2e8b66d1a9763a12161ed94a33
Ezequiel Bugnon <bugnonezequiel@gmail.com>
2025-09-18 20:21:42 -0300

    feat(app): add infix and prefix methods parser

--- parser/parser.go
+++ parser/parser.go
@@ -7,11 +7,19 @@ import (
 	"leetcode/src/compiler-in-go/token"
 )
 
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(ast.Expression) ast.Expression
+)
+
 type Parser struct {
 	l         *lexer.Lexer
 	curToken  token.Token
 	peekToken token.Token
 	errors    []string
+
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
 }
 
 func New(l *lexer.Lexer) *Parser {
